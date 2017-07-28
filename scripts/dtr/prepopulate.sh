c3dfb5b931bb127f202700d480e6b6f982e5c781
Aaron Miller <aaron.miller@docker.com>
2017-07-28 10:59:31 -0400

    Fix dtr prepopulate get token script name, in-progress changes

--- scripts/dtr/prepopulate.sh
+++ scripts/dtr/prepopulate.sh
@@ -1,9 +1,9 @@
 # Variable Assignment
-tld=$(grep 'tld:' /vagrant/config.yaml | awk '{ print $2}')
-password=$(grep 'password:' /vagrant/config.yaml | awk '{ print $2}')
+tld=$(grep 'tld:' /vagrant/config.yaml | awk '{print $2}')
+password=$(grep 'password:' /vagrant/config.yaml | awk '{print $2}')
 
 # Get an auth token and save in config.yaml
-/vagrant/scripts/get_ucp_token.sh
+/vagrant/scripts/ucp/get_auth_token.sh
 
 # Create organizations
 echo "Populating DTR with sample organizations"
@@ -27,8 +27,8 @@ echo "Adding April, Bob and Charlie to the dev team"
 echo "Populating DTR with sample repositories"
 /vagrant/scripts/dtr/create_repo.sh "it" "nginx" "public" "IT approved NGINX"
 /vagrant/scripts/dtr/create_repo.sh "it" "node" "public" "IT approved Node.js"
-/vagrant/scripts/dtr/create_repo.sh "dev" "node-web-app" "public"
-/vagrant/scripts/dtr/create_repo.sh "prod" "node-web-app" "public"
+/vagrant/scripts/dtr/create_repo.sh "dev" "whalesay" "public"
+/vagrant/scripts/dtr/create_repo.sh "prod" "demo-flask-app" "public"
 
 # Add team access to repositories
 echo "Adding team access to repositories"
@@ -37,12 +37,12 @@ echo "Adding team access to repositories"
 # Populate repositories with images
 echo "Loading images in DTR"
 docker login dtr.${tld} -u admin -p ${password}
-docker pull nginx
-docker pull node
-docker pull bkauf/node-web-app
-docker tag nginx dtr.${tld}/it/nginx
-docker tag node dtr.${tld}/it/node
-docker tag bkauf/node-web-app dtr.${tld}/dev/node-web-app
+docker pull nginx:alpine
+docker pull node:alpine
+docker pull dockeramiller/whalesay
+docker tag nginx:alpine dtr.${tld}/it/nginx
+docker tag node:alpine dtr.${tld}/it/node
+docker tag dockeramiller/whalesay dtr.${tld}/dev/whalesay
 docker push dtr.${tld}/it/nginx
 docker push dtr.${tld}/it/node
-docker push dtr.${tld}/dev/node-web-app
+docker push dtr.${tld}/dev/whalesay
