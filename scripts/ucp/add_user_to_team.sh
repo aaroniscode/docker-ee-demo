a157ba6c7c4c7515b1e9e4f4e89023db44995ee4
Aaron Miller <aaron.miller@docker.com>
2017-08-15 12:38:09 -0400

    Update add_user_to_team.sh for 17.06

--- scripts/ucp/add_user_to_team.sh
+++ scripts/ucp/add_user_to_team.sh
@@ -1,9 +1,11 @@
 # Variable Assignment
 tld=$(grep 'tld:' /vagrant/config.yaml | awk '{print $2}')
 token=$(grep 'ucp_token:' /vagrant/config.yaml | awk '{print $2}')
-team_id=$1
-user=$2
+org=$1
+team=$2
+user=$3
 
 curl -sk -H "Authorization: Bearer $token" \
   -H "Content-Type: application/json;charset=UTF-8" \
-  -X PUT https://ucp.${tld}/api/teams/${team_id}/members/add/${user}
+  -d "{}" \
+  -X PUT https://ucp.${tld}/accounts/{$org}/teams/${team}/members/${user}
