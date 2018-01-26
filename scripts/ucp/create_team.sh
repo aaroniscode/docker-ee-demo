2be8b79cf9b4c1e803922973a2e25359d135932f
Aaron Miller <aaron.miller@docker.com>
2018-01-26 13:24:55 -0500

    Allow create_team script to add to other orgs

--- scripts/ucp/create_team.sh
+++ scripts/ucp/create_team.sh
@@ -1,9 +1,10 @@
 # Variable Assignment
 tld=$(grep 'tld:' /vagrant/config.yaml | awk '{print $2}')
 token=$(grep 'ucp_token:' /vagrant/config.yaml | awk '{print $2}')
-team_name=$1
+org_name=$1
+team_name=$2
 
 curl -sk -H "Authorization: Bearer $token" \
   -H "Content-Type: application/json;charset=UTF-8" \
   -d "{\"name\": \"${team_name}\"}", \
-    "https://ucp.${tld}/enzi/v0/accounts/docker-datacenter/teams"
+    "https://ucp.${tld}/enzi/v0/accounts/${org_name}/teams"
